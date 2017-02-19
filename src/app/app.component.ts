96f5c4a0aaa883bfad7939b0683b234b2d2fd5ad
Mentor <lolipopsqanglais@gmail.com>
2017-02-20 00:24:56 +0100

    Display list of selectable heroes; Show selected hero's details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,38 @@ export class Hero{
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
+
 @Component({
   selector: 'my-app',
   template: `
       <h1>{{title}}</h1>
       <h2>My Heroes</h2>
       <ul class="heroes">
-        <li *ngFor="let hero of heroes">
+        <li *ngFor="let hero of heroes" 
+            [class.selected]="hero === selectedHero"
+            (click)="onSelect(hero)">
           <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
       </ul>
-      
-      <h2>{{hero.name}} details!</h2>
-      <div><label for="id">id: </label>{{hero.id}}</div>
-      <div>
-          <label for="name">name: </label>
-          <input [(ngModel)]="hero.name" placeholder="name">
+      <div *ngIf="selectedHero">
+        <h2>{{selectedHero.name}} details!</h2>
+        <div><label for="id">id: </label>{{selectedHero.id}}</div>
+        <div>
+            <label for="name">name: </label>
+            <input [(ngModel)]="selectedHero.name" placeholder="name">
+        </div>
       </div>
       `,
   styles: [`
@@ -76,23 +92,13 @@ export class Hero{
 })
 export class AppComponent  {
   title = 'Tour of Heroes';
-  hero:Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
-];
+
 
