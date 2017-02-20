507611b4fb26cd99d918e5c583a8f29fa6859c85
Mentor <mentor.bajraktari@free.fr>
2017-02-20 11:32:19 +0100

    Dependency injection : inject HeroService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { HeroService } from "./hero.service";
 import { Hero } from "./hero";
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -63,17 +65,31 @@ import { Hero } from "./hero";
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+  providers: [ HeroService ]
 
 })
 
-export class AppComponent  {
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService: HeroService){
+
+  }
+
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
 
